4e5a4b14c41b26dd155392070b3bfcd0009e0e25
Steven Linn <smlucf@gmail.com>
2024-02-23 19:01:17 -0700

    Add cypress correctly

---
+++ tests/cypress.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  reporter: "junit",
+
+  reporterOptions: {
+    mochaFile: "test-results/test-output-[hash].xml",
+  },
+
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  record: true,
+  video: true,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
