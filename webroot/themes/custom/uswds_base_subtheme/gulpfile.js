f439991221987593c17d02509a617a61116a0c93
Steven Linn <smlucf@gmail.com>
2024-02-26 11:03:44 -0700

    Enable dev settings and remove assets

--- webroot/themes/custom/uswds_base_subtheme/gulpfile.js
+++ webroot/themes/custom/uswds_base_subtheme/gulpfile.js
@@ -1,27 +1,8 @@
-/*
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-========================================
-========================================
-========================================
-----------------------------------------
-USWDS SASS GULPFILE
-----------------------------------------
-*/
-const uswds = require("@uswds/compile");
+/* gulpfile.js */
+const { src, dest, series, parallel, watch } = require("gulp");
 
-// Custom variables
-const pkg = require('./package.json');
-const {series, watch, src} = require("gulp");
-const gulp = require('gulp');
-const sassLint = require('gulp-sass-lint');
-const eslint = require('gulp-eslint');
-const rename = require("gulp-rename");
-const path = require("path");
-let uglify = require('gulp-uglify-es').default;
-const del = require("del");
+// eslint-disable-next-line import/no-unresolved
+const uswds = require('@uswds/compile');
 
 /**
  * USWDS version
@@ -34,106 +15,19 @@ uswds.settings.version = 3;
  * Set as many as you need
  */
 
-// Where to output
-uswds.paths.dist.css = './css';
-
-// Custom scss files
-uswds.paths.dist.custom = './components/**';
-
+uswds.paths.dist.css = './assets/css';
+uswds.paths.dist.theme = './components';
 uswds.paths.dist.fonts = './assets/fonts';
 uswds.paths.dist.img = './assets/img';
 uswds.paths.dist.js = './assets/js';
-uswds.paths.dist.scss = './assets/scss';
-
-uswds.paths.src.projectSass = './components/**';
-
-// Custom Functions
-
-function cleanJs() {
-  del([pkg.paths.dist.js]);
-  return Promise.resolve('js folder deleted.');
-}
-
-function cleanCss() {
-  del([pkg.paths.dist.css]);
-  return Promise.resolve('css folder deleted.');
-}
-
-function scssLint() {
-  return src(pkg.paths.scss)
-      .pipe(sassLint())
-      .pipe(sassLint.format())
-}
-
-function scssLintCi() {
-  return src(pkg.paths.scss)
-      .pipe(sassLint())
-      .pipe(sassLint.format())
-      .pipe(sassLint.failOnError())
-}
-
-// After we let uswds compile build the css files we need to minify them.
-function minifyCss() {
-  return src(pkg.paths.css)
-      // .pipe(rename(function (file) {
-      //   // this removes the last parent directory of the relative file path
-      //   let parts = file.dirname.split("/");
-      //   parts = parts.slice(1);
-      //   parts = parts.join("/");
-      //   file.dirname = path.dirname(parts + "/" + file.basename);
-      //   console.log("file.dirname = " + file.dirname);
-      // }))
-      .pipe(rename({"suffix": '.min'}))
-      .pipe(gulp.dest(pkg.paths.dist.css));
-}
-
-function buildJs() {
-  return src(pkg.paths.js)
-      .pipe(rename(function (file) {
-        // this removes the last parent directory of the relative file path
-        let parts = file.dirname.split("/");
-        parts = parts.slice(1);
-        parts = parts.join("/");
-        file.dirname = path.dirname(parts + "/" + file.basename);
-      }))
-      .pipe(rename({"suffix": '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest(pkg.paths.dist.js));
-}
-
-function jsLint() {
-  return src(pkg.paths.js)
-      .pipe(eslint())
-      .pipe(eslint.format())
-}
-
-function jsLintCi() {
-  return src(pkg.paths.js)
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
-}
+uswds.paths.dist.scss = './components';
 
 /**
  * Exports
  * Add as many as you need
  */
 
-// copyAll + compile
 exports.init = uswds.init;
-
-exports.compileSass = series(cleanCss, scssLint, uswds.compileSass, minifyCss);
-exports.compileJs = series(cleanJs, jsLint, buildJs);
-exports.compileAll = series(this.compileJs, this.compileSass);
-
-exports.lint = series(scssLint, jsLint);
-
-exports.lintCi = series(scssLintCi, jsLintCi);
-
-exports.watch = series(this.compileAll, () => {
-  watch(pkg.paths.scss, series([this.compileSass]));
-  watch(pkg.paths.js, series([this.compileJs]));
-});
-exports.updateUswds = uswds.updateUswds;
-
-exports.default = series(this.init, this.compileAll);
+exports.compile = series(uswds.copyAssets, uswds.compile);
+exports.watch = uswds.watch;
+exports.default = exports.compile;
